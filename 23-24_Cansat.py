ee5af75adc1f156482c51bdd13826cef9cc71bb4
steeleelliott03 <100764769+steeleelliott03@users.noreply.github.com>
2023-10-16 19:09:37 -0400

    Added instructions for setting up CanSat class for storing data.

--- 23-24_Cansat.py
+++ 23-24_Cansat.py
@@ -3,6 +3,17 @@
 # Danush Singla :))
 # Matthew Lee 
 # Alex Segelnick
+
+
+# TODO: Import necessary libraries
+# Class for encapsulating each data point being displayed, with functions that update each data point
+class CanSat:
+    # TODO: Initialize each variable based on the ones listed in the resources
+    def __init__(self):
+        self.altitude = 0
+        self.temperature = 0
+        self.pressure = 0
+
 def main():
     print("Telemetry Team Rules!")
 
