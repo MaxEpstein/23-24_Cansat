5f5d4ba075d6f6e914dc598b0fc57ec6cd5c880f
steeleelliott03 <100764769+steeleelliott03@users.noreply.github.com>
2023-10-23 18:59:15 -0400

    Updated class initialization

--- 23-24_Cansat.py
+++ 23-24_Cansat.py
@@ -21,24 +21,30 @@ class graphs:
 class CanSat:
     # TODO: Initialize each variable based on the ones listed in the resources
     def __init__(self):
-        self.ALTITUDE = 0
-        self.TEMPERTAURE = 0
-        self.PRESSURE = 0
-
-         # Variables used for the first row of the GUI
+        # Variables used for the first row of the GUI
         self.TEAM_ID = 2031 #Team ID, given by the contest
         self.MISSION_TIME = '00:00:00' # In UTC time in hh:mm:ss
-
-        # Variables used for the second row of the GUI 
-        self.PC_DEPLOY = 'N' # 'C' indicates the parachute is deployed (at 100 m), 'N' otherwise.
-        self.MODE = 'S' # 'F' for flight mode and 'S' for simulation mode.
-        self.GPS_TIME = '00:00:00' # Is the time from the GPS receiver. The time must be reported in UTC and have a resolution of a second.
-        self.SS1 = 'U' # Software State - Tells the state of the rocket - 'LAUNCH_WAIT', 'ASCENT', 'ROCKET_SEPARATION', 'DESCENT', 'HS_RELEASE', 'LANDED', or 'U' (undetermined)
-
-        # Variables used for the third row of the GUI 
+        
         self.PACKET_COUNT = 0 # Number of packets
-        self.HS_DEPLOY = 'N' # 'P' indicates the heat shield is deployed, 'N' otherwise.
+        self.MODE = 'S' # 'F' for flight mode and 'S' for simulation mode.
+        self.STATE = 'U' # 'L' for launchpad, 'A' for ascent, 'D' for descent, 'G' for gliding, 'P' for parachute, 'L' for landed, 'U' for unknown
+        self.ALTITUDE = 0 # Altitude in meters
+        self.AIR_SPEED = 0 # Air speed in meters per second
+        self.HS_DEPLOYED = 'N' # 'P' if the heat shield is deployed, 'N' otherwise
+        self.PC_DEPLOYED = 'N' # 'C' indicates the parachute is deployed (at 100 m), 'N' otherwise.
+        self.TEMPERATURE = 0 # Temperature in degrees Celsius
+        self.PRESSURE = 0 # Pressure in kilopascals
+        self.VOLTAGE = 0 # Voltage in volts
+        self.GPS_TIME = '00:00:00' # Is the time from the GPS receiver. The time must be reported in UTC and have a resolution of a second.
+        self.GPS_ALTITUDE = 0 # Is the altitude from the GPS receiver. The altitude must be in meters and have a resolution of a meter.
+        self.GPS_LATITUDE = 0
+        self.GPS_LONGITUDE = 0
         self.GPS_SATS = 0 # Is the number of GPS satellites being tracked by the GPS receiver. This must be an integer.
+        self.TILT_X = 0 # Is the tilt angle of the CanSat along the X axis. The tilt angle must be in degrees and have a resolution of a degree.
+        self.TILT_Y = 0 # Is the tilt angle of the CanSat along the Y axis. The tilt angle must be in degrees and have a resolution of a degree.
+        self.TILT_Z = 0 # Is the tilt angle of the CanSat along the Z axis. The tilt angle must be in degrees and have a resolution of a degree.
+        self.ROT_Z = 0 # Is the rotation angle of the CanSat along the Z axis. The rotation angle must be in degrees and have a resolution of a degree.
+        
         self.CMD_ECHO = "CXON" # Is the text of the last command received and processed by the Cansat. For example, CXON or SP101325. See the command section for details of command formats. Do not include com characters.
 
 
