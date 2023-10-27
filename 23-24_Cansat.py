6862eca3e0f3fc611ba43e5b8b3203e3def8512c
steeleelliott03 <100764769+steeleelliott03@users.noreply.github.com>
2023-10-27 10:54:34 -0400

    Reorganized gui functionality to be called from class. Event handling will be done in the run_gui() function inside the CanSat class. I also think using functions to create and return individual elements in the gui may be helpful for debugging and organization.

--- 23-24_Cansat.py
+++ 23-24_Cansat.py
@@ -48,58 +48,59 @@ class CanSat:
         self.CMD_ECHO = "CXON" # Is the text of the last command received and processed by the Cansat. For example, CXON or SP101325. See the command section for details of command formats. Do not include com characters.
 
 
-
-
-def main():
-    # List of graphs needed
-    list_of_graphs = ['Altitude (m) vs. Time (s)',
-                      'Temp (c) vs. Time (s)',
-                      'Voltage (volt) vs Time (s)',
-                      'Tilt X (deg) vs Time (s)',
-                      'Tilt Y (deg) vs Time (s)',
-                      'GPS Altitude (deg) vs Time (s)',
-                      'GPS Latitude (deg) vs Time (s)',
-                      'GPS Longitude (deg) vs Time (s)',
-                      'Acceleration (m/s^2) vs Time (s)']
-    
-    cansat = CanSat()
-
-    # Sets the color theme of the dashboard 
-    sg.theme('DarkAmber')
-
-    # All the stuff inside your window.
-    top_banner = [sg.Text('Team ID: '+str(cansat.TEAM_ID), font='Any 26', background_color='#1B2838', border_width=(5), size=(40), key = 'TEAM_ID'),
-            sg.Text(cansat.MISSION_TIME, font='Any 22', background_color='#1B2838', border_width=(8), size=(10), key = 'missionTime'),
+    def create_top_banner(self):
+        return [
+            sg.Text('Team ID: '+str(self.TEAM_ID), font='Any 26', background_color='#1B2838', border_width=(5), size=(40), key='TEAM_ID'),
+            sg.Text(self.MISSION_TIME, font='Any 22', background_color='#1B2838', border_width=(8), size=(10), key='missionTime'),
             sg.Button('Calibrate', font='Any 16'),
             sg.Button('Connect', font='Any 16'),
-            sg.Button('Close', font='Any 16')]
-    
-    second_row = [sg.Text('PC DEPOY: '+ cansat.PC_DEPLOYED, size=(14), font='Any 16', background_color='#1B2838', key = 'PC_DEPLOY'),
-            sg.Text('Mode: '+ cansat.MODE, size=(13), font='Any 16', background_color='#1B2838', key = 'MODE'),
-            sg.Text('GPS Time: ' + cansat.GPS_TIME, size=(18), font='Any 16', background_color='#1B2838', key='gpsTime'),
-            sg.Text('Software State : '+cansat.STATE, size=(32), font='Any 16', background_color='#1B2838', key = 'STATE')]
-    
-    third_row = [sg.Text('Packet Count: '+str(cansat.PACKET_COUNT), size=(17), font='Any 16', background_color='#1B2838', key = 'PC1'),
-            sg.Text('HS Deploy: '+cansat.HS_DEPLOYED, size=(15), font='Any 16', background_color='#1B2838', key = 'HS_DEPLOY'),
-            sg.Text('GPS Sat: ' +str(cansat.GPS_SATS), size=(13), font='Any 16', background_color='#1B2838', key = 'GPS_SAT'),
-            sg.Text('CMD Echo: '+cansat.CMD_ECHO, size=(25), font='Any 16', background_color='#1B2838', key = 'CMD_ECHO')]
-    
-    layout = [top_banner,
-              second_row,
-              third_row]
-    
-    # Create the Window
-    window = sg.Window('Window Title', layout)
+            sg.Button('Close', font='Any 16')
+        ]
 
-    # Event Loop to process "events" and get the "values" of the inputs
-    while True:
-        event, values = window.read()
-        if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
-            break
-        print('You entered ', values[0])
+    def create_second_row(self):
+        return [
+            sg.Text('PC DEPOY: '+ self.PC_DEPLOYED, size=(14), font='Any 16', background_color='#1B2838', key='PC_DEPLOY'),
+            sg.Text('Mode: '+ self.MODE, size=(13), font='Any 16', background_color='#1B2838', key='MODE'),
+            sg.Text('GPS Time: ' + self.GPS_TIME, size=(18), font='Any 16', background_color='#1B2838', key='gpsTime'),
+            sg.Text('Software State : '+self.STATE, size=(32), font='Any 16', background_color='#1B2838', key='STATE')
+        ]
 
-    # Closes the window 
-    window.close()
+    def create_third_row(self):
+        return [
+            sg.Text('Packet Count: '+str(self.PACKET_COUNT), size=(17), font='Any 16', background_color='#1B2838', key='PC1'),
+            sg.Text('HS Deploy: '+self.HS_DEPLOYED, size=(15), font='Any 16', background_color='#1B2838', key='HS_DEPLOY'),
+            sg.Text('GPS Sat: ' +str(self.GPS_SATS), size=(13), font='Any 16', background_color='#1B2838', key='GPS_SAT'),
+            sg.Text('CMD Echo: '+self.CMD_ECHO, size=(25), font='Any 16', background_color='#1B2838', key='CMD_ECHO')
+        ]
+
+    def create_gui_layout(self):
+        top_banner = self.create_top_banner()
+        second_row = self.create_second_row()
+        third_row = self.create_third_row()
+        
+        layout = [
+            top_banner,
+            second_row,
+            third_row
+        ]
+        return layout
+
+    def run_gui(self):
+        sg.theme('DarkAmber')
+        layout = self.create_gui_layout()
+        window = sg.Window('Window Title', layout)
+
+        while True:
+            event, values = window.read()
+            if event == sg.WIN_CLOSED or event == 'Cancel':
+                break
+            print('You entered ', values[0])
+
+        window.close()
+
+def main():
+    cansat = CanSat()
+    cansat.run_gui()
 
 if __name__=='__main__':
     main()
