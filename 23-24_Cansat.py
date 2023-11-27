adaf79de24f4a713d14519531989c025748860ef
steeleelliott03 <100764769+steeleelliott03@users.noreply.github.com>
2023-11-27 16:09:28 -0500

    Updating gui elements and graphs slightly better

--- 23-24_Cansat.py
+++ 23-24_Cansat.py
@@ -35,17 +35,13 @@ class CanSat:
             'GPS_SATS': 0,
             'TILT_X': 0,
             'TILT_Y': 0,
-            'TILT_Z': 0,
             'ROT_Z': 0,
             'CMD_ECHO': "CXON"
         }
-
-        self.fig, self.ax = plt.subplots(figsize=(6, 3))
-        self.canvas_elem = FigureCanvasTkAgg(self.fig, master=None)
-        self.layout = self.create_gui_layout()
         self.csv_file_path = csv_file_path
         self.df = pd.read_csv(self.csv_file_path)
-        
+        self.layout = self.create_gui_layout()
+            
     def init_graphs(self):
         self.graphs = {
             'altitude': plt.subplots(figsize=(3.5, 3.5)),
@@ -53,7 +49,7 @@ class CanSat:
             'voltage': plt.subplots(figsize=(3.5, 3.5)),
             'tilt_x': plt.subplots(figsize=(3.5, 3.5)),
             'tilt_y': plt.subplots(figsize=(3.5, 3.5)),
-            'tilt_z': plt.subplots(figsize=(3.5, 3.5)),
+            'rot_z': plt.subplots(figsize=(3.5, 3.5)),
             'gps_altitude': plt.subplots(figsize=(3.5, 3.5)),
             'gps_latitude': plt.subplots(figsize=(3.5, 3.5)),
             'gps_longitude': plt.subplots(figsize=(3.5, 3.5))
@@ -72,14 +68,6 @@ class CanSat:
             ax.set_xlabel('Time')
             ax.set_ylabel(key.capitalize())
             ax.set_title(f'{key.capitalize()} vs Time')
-               
-    def create_graph_canvas(self):
-        return sg.Canvas(
-            key='graph_canvas',
-            background_color='white',
-            size=(400, 200),
-            pad=(10, 10)
-        )
 
     def display_all_graphs(self):
         for key, (fig, ax) in self.graphs.items():
@@ -90,7 +78,7 @@ class CanSat:
     def create_top_banner(self):
         return [
             sg.Text('Team ID: ' + str(self.data['TEAM_ID']), font=('Helvetica', 16), background_color='#1B2838', text_color='white', size=(20, 1), justification='left', key='TEAM_ID'),
-            sg.Text(self.data['MISSION_TIME'], font=('Helvetica', 16), background_color='#1B2838', text_color='white', size=(10, 1), justification='right', key='missionTime'),
+            sg.Text(self.data['MISSION_TIME'], font=('Helvetica', 16), background_color='#1B2838', text_color='white', size=(10, 1), justification='right', key='MISSION_TIME'),
             sg.Button('Calibrate', font=('Helvetica', 12)),
             sg.Button('Connect', font=('Helvetica', 12)),
             sg.Button('Close', font=('Helvetica', 12))
@@ -100,7 +88,7 @@ class CanSat:
         return [
             sg.Text('PC DEPLOY: ' + self.data['PC_DEPLOYED'], font=('Helvetica', 14), background_color='#1B2838', text_color='white', size=(12, 1), justification='left', key='PC_DEPLOY'),
             sg.Text('Mode: ' + self.data['MODE'], font=('Helvetica', 14), background_color='#1B2838', text_color='white', size=(12, 1), justification='left', key='MODE'),
-            sg.Text('GPS Time: ' + self.data['GPS_TIME'], font=('Helvetica', 14), background_color='#1B2838', text_color='white', size=(16, 1), justification='left', key='gpsTime'),
+            sg.Text('GPS Time: ' + self.data['GPS_TIME'], font=('Helvetica', 14), background_color='#1B2838', text_color='white', size=(16, 1), justification='left', key='GPS_TIME'),
             sg.Text('Software State: ' + self.data['STATE'], font=('Helvetica', 14), background_color='#1B2838', text_color='white', size=(30, 1), justification='left', key='STATE')
         ]
 
@@ -149,74 +137,105 @@ class CanSat:
             fifth_row, 
             sixth_row,
 
-            [self.create_graph_canvas()],  # Add the graph canvas to the layout
         ]
         return layout
 
     def set_data(self, data_dict):
         self.data.update(data_dict)
 
-    def get_details(self):
-        keys = ['MISSION_TIME', 'PACKET_COUNT', 'MODE', 'STATE', 'HS_DEPLOYED', 'PC_DEPLOYED']
-        return self.get_values(keys)
-
-    def get_measurements(self):
-        keys = ['ALTITUDE', 'AIR_SPEED', 'TEMPERATURE', 'PRESSURE', 'VOLTAGE']
-        return self.get_values(keys)
-
-    def get_GPS(self):
-        keys = ['GPS_TIME', 'GPS_ALTITUDE', 'GPS_LATITUDE', 'GPS_LONGITUDE', 'GPS_SATS']
-        return self.get_values(keys)
-
-    def get_direction(self):
-        keys = ['TILT_X', 'TILT_Y', 'TILT_Z', 'ROT_Z']
-        return self.get_values(keys)
-
-    def get_values(self, keys):
-        return {key: self.data[key] for key in keys}
-
     def run_gui(self):
         sg.theme('DarkBlue3')
         self.window = sg.Window('CanSat Dashboard', self.layout, finalize=True)
         self.init_graphs()
-
         while True:
-            event, values = self.window.read(timeout=1000)  # Example: Update every second
+            event, values = self.window.read(timeout=1000)
             if event == sg.WIN_CLOSED or event == 'Close':
                 break
 
-            # Simulate data update; replace this with actual data reading logic
-            new_data = self.simulate_data_update()
+            new_data = self.read_latest_csv_data()  # Read latest data from CSV
             self.update_graphs(new_data)
             self.display_all_graphs()
+            self.update_gui_elements()
 
         self.window.close()
 
+
     # Placeholder for data update simulation
-    def simulate_data_update(self):
-        # Read the CSV file
-        df = pd.read_csv(self.csv_file_path)
+    def read_latest_csv_data(self):
+        # Re-read the CSV file to get the latest data
+        self.df = pd.read_csv(self.csv_file_path)
 
-        # You can use the tail method to get the most recent data, for simulation purposes
-        latest_data = df.tail(1)
+        # Get the latest row
+        latest_data = self.df.iloc[-1]
 
-        # Extract the necessary columns and convert them to the appropriate format
-        # Adjust the keys according to your graph labels
-        simulation_data = {
-            'time': [latest_data['MISSION_TIME'].iloc[0]],
-            'altitude': [latest_data['ALTITUDE'].iloc[0]],
-            'temperature': [latest_data['TEMPERATURE'].iloc[0]],
-            'voltage': [latest_data['VOLTAGE'].iloc[0]],
-            'tilt_x': [latest_data['TILT_X'].iloc[0]],
-            'tilt_y': [latest_data['TILT_Y'].iloc[0]],
-            'tilt_z': [latest_data['TILT_Z'].iloc[0]],
-            'gps_altitude': [latest_data['GPS_ALTITUDE'].iloc[0]],
-            'gps_latitude': [latest_data['GPS_LATITUDE'].iloc[0]],
-            'gps_longitude': [latest_data['GPS_LONGITUDE'].iloc[0]],
-            # Add other necessary fields here
+        # Update the self.data dictionary with the latest data
+        self.data['MISSION_TIME'] = latest_data['MISSION_TIME']
+        self.data['PACKET_COUNT'] = latest_data['PACKET_COUNT']
+        self.data['MODE'] = latest_data['MODE']
+        self.data['STATE'] = latest_data['STATE']
+        self.data['ALTITUDE'] = latest_data['ALTITUDE']
+        self.data['AIR_SPEED'] = latest_data['AIR_SPEED']
+        self.data['HS_DEPLOYED'] = latest_data['HS_DEPLOYED']
+        self.data['PC_DEPLOYED'] = latest_data['PC_DEPLOYED']
+        self.data['TEMPERATURE'] = latest_data['TEMPERATURE']
+        self.data['PRESSURE'] = latest_data['PRESSURE']
+        self.data['VOLTAGE'] = latest_data['VOLTAGE']
+        self.data['GPS_TIME'] = latest_data['GPS_TIME']
+        self.data['GPS_ALTITUDE'] = latest_data['GPS_ALTITUDE']
+        self.data['GPS_LATITUDE'] = latest_data['GPS_LATITUDE']
+        self.data['GPS_LONGITUDE'] = latest_data['GPS_LONGITUDE']
+        self.data['GPS_SATS'] = latest_data['GPS_SATS']
+        self.data['TILT_X'] = latest_data['TILT_X']
+        self.data['TILT_Y'] = latest_data['TILT_Y']
+        self.data['ROT_Z'] = latest_data['ROT_Z']
+        self.data['CMD_ECHO'] = latest_data['CMD_ECHO']
+
+        # Create a dictionary to hold the data for the graphs
+        graph_data = {
+            'time': [latest_data['MISSION_TIME']],
+            'altitude': [latest_data['ALTITUDE']],
+            'temperature': [latest_data['TEMPERATURE']],
+            'voltage': [latest_data['VOLTAGE']],
+            'tilt_x': [latest_data['TILT_X']],
+            'tilt_y': [latest_data['TILT_Y']],
+            'rot_z': [latest_data['ROT_Z']],
+            'gps_altitude': [latest_data['GPS_ALTITUDE']],
+            'gps_latitude': [latest_data['GPS_LATITUDE']],
+            'gps_longitude': [latest_data['GPS_LONGITUDE']]
+            # Add other fields if needed
         }
 
-        return simulation_data
+        return graph_data
+
+
+
+    def update_gui_elements(self):
+        # Update GUI elements with the latest data
+        self.window['TEAM_ID'].update('Team ID: ' + str(self.data['TEAM_ID']))
+        self.window['MISSION_TIME'].update('Mission Time: ' + self.data['MISSION_TIME'])
+        self.window['PC1'].update('Packet Count: ' + str(self.data['PACKET_COUNT']))
+        self.window['MODE'].update('Mode: ' + self.data['MODE'])
+        self.window['STATE'].update('Software State: ' + self.data['STATE'])
+        self.window['PC_DEPLOY'].update('PC Deploy: ' + self.data['PC_DEPLOYED'])
+        self.window['HS_DEPLOY'].update('HS Deploy: ' + self.data['HS_DEPLOYED'])
+        self.window['GPS_SAT'].update('GPS Sat: ' + str(self.data['GPS_SATS']))
+        self.window['CMD_ECHO'].update('CMD Echo: ' + self.data['CMD_ECHO'])
+        self.window['GPS_TIME'].update('GPS Time: ' + self.data['GPS_TIME'])
+        # Add additional updates for other parameters
+        '''
+        self.window['ALTITUDE'].update('Altitude: ' + str(self.data['ALTITUDE']))
+        self.window['AIR_SPEED'].update('Air Speed: ' + str(self.data['AIR_SPEED']))
+        self.window['TEMPERATURE'].update('Temperature: ' + str(self.data['TEMPERATURE']))
+        self.window['PRESSURE'].update('Pressure: ' + str(self.data['PRESSURE']))
+        self.window['VOLTAGE'].update('Voltage: ' + str(self.data['VOLTAGE']))
+        self.window['GPS_ALTITUDE'].update('GPS Altitude: ' + str(self.data['GPS_ALTITUDE']))
+        self.window['GPS_LATITUDE'].update('GPS Latitude: ' + str(self.data['GPS_LATITUDE']))
+        self.window['GPS_LONGITUDE'].update('GPS Longitude: ' + str(self.data['GPS_LONGITUDE']))
+        self.window['TILT_X'].update('Tilt X: ' + str(self.data['TILT_X']))
+        self.window['TILT_Y'].update('Tilt Y: ' + str(self.data['TILT_Y']))
+        self.window['TILT_Z'].update('Tilt Z: ' + str(self.data['TILT_Z']))
+        self.window['ROT_Z'].update('Rot Z: ' + str(self.data['ROT_Z']))
+'''
 
 
 def main():
